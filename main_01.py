e516d2ce6b6405e69d3ebed6a1f2a89bf200503e
Neflibata-A <2521318215@qq.com>
2024-05-09 01:00:54 +0800

    Add files via upload

---
+++ main_01.py
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+int main()
+{
+	printf("The last card is: DK\n"
+		"Last A card : A3\n"
+		"Last A card : A5\n"
+		"Last A card : A2\n"
+		"Last A card : A1\n"
+		"Last A card : A4\n");
+
+
+
+
+
+
+
+	return EXIT_SUCCESS;
+}
